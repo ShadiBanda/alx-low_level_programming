624608a620f2bf906af105adbcdc73c2211672c2
root <Mashadibanda94@gmail.com>
2023-03-16 14:12:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - is the main function
+*main - is the main function
 *
-* Return: is always 0
+*Return: is always 0
 */
-int maun(void)
-{puts("\"Programming is like building a multilingual puzzle");
-return (0); }	
+int main(void)
 
+{ puts("\"Programming is like building a multilingual puzzle");
+return (0); }
