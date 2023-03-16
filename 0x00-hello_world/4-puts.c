3afac4fdcdafceb38f342cf802a45916c84dc086
root <Mashadibanda94@gmail.com>
2023-03-16 14:01:46 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - is the main function
+*
+* Return: is always 0
+*/
+int maun(void)
+{puts("\"Programming is like building a multilingual puzzle");
+return (0); }	
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main - Is the main function
+ * Return: 0 when successfull
+*/
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,");
+return (0);}       
