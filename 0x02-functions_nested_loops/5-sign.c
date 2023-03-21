881880d9124bf86b08c322bcab125dc5191a3b2d
root <Mashadibanda94@gmail.com>
2023-03-21 12:38:07 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,14 +16,14 @@ int print_sign(int n)
 		_putchar(' ');
 		return (1);
 	}
-else if (n == 0)
+	else if (n == 0)
 	{
-	        _putchar('0');
+		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
 		return (0);
 	}
-else
+	else
 	{
 		_putchar('-');
 		_putchar(',');
@@ -31,4 +31,3 @@ else
 		return (-1);
 	}
 }
-
