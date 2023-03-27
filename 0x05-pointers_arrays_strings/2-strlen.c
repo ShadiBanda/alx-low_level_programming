40919da0ad9ae8b39f32eced4662419e322b7568
root <Mashadibanda94@gmail.com>
2023-03-27 10:43:25 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,19 @@
- #include "main.h"
-
+#include "main.h"
 /**
- * _puts - prints a string
- * @str: the string
- *
- * Return: the length of the string
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
-void _puts(char *str)
+int _strlen(char *s)
 {
-	int i = 0;
+	int longi = 0;
 
-	while (str[i] != '\0')
+	while (*s != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		longi++;
+		s++;
 	}
-	_putchar('\n');
+
+	return (longi);
 }
 
