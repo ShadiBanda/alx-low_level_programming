938518b4343da4967120ea08b7c9516949c2a5b1
root <Mashadibanda94@gmail.com>
2023-04-04 13:03:35 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,9 +5,10 @@
  * @needle: input
  * Return: Always 0 (Success)
  */
-char *_strstr(char *haystack, char *needle)
+char *_strstr (char *haystack, char *needle);
 {
-		for (; *haystack != '\0'; haystack++)
+
+	for (; *haystack != '\0'; haystack++;)
 	{
 		char *l = haystack;
 		char *p = needle;
