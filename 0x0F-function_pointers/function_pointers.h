848533f8d89e5e0c8ea7b62111d2e95e111d578f
root <Mashadibanda94@gmail.com>
2023-04-19 17:19:51 -0700

    3-main.c CODE

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,16 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-#include <stddef.h>
-#include <stdlib.h>
+/*function_pointers.h*/
 
-void print_name(char *name, void (*f)(char *));
+#ifndef FUNCTION_POINTERS_
+#define FUNCTION_POINTERS_
+
+
+#include <stddef.h>
+
+
 int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+
+#endif /*function_pointers_h*/
