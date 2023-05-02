f81e74467bc70714128db5f12247e2e48660bd0f
root <Mashadibanda94@gmail.com>
2023-05-02 14:49:20 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,9 +11,9 @@ void free_list(list_t *head)
 	list_t *current;
 
 	while ((current = head) != NULL)
-    {
-        head = head->next;
-        free(current->str);
-        free(current);
-    }
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
 }
