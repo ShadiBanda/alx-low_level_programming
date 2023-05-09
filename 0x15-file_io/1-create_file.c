075590768baef3790d261c0a41193b8f8202b58c
root <Mashadibanda94@gmail.com>
2023-05-08 17:23:27 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,21 +8,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len = 0;
+	int fd, wr, len = 0;
 
-	if (filename == NULL)
+	if (filename)
 		return (-1);
 
-	if (text_content != NULL)
+	if (!text_content)
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	write(fd, text_content, len);
+	wr = write(fd, text_content, len);
 
-	if (fd == -1)
+	if (fd == -1 || wr == -1)
 		return (-1);
 
 	close(fd);
