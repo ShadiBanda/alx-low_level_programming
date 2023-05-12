7d1ce85946aaf8be9959c48f922c8660bdfba380
ShadiBanda <mashadibanda94@gmail.com>
2023-05-12 05:20:38 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,44 @@
 #include "main.h"
 
 /**
- *create_file - Creates a file
- * @filename - Pointer to the name of the file to create
- * @text_content - A pointer to a string to write to the file
- * Return: returns 1 on success, -1 on failure
+ * create_file - Creates a new file and writes text to it
+ * @filename: A pointer to a constant character
+ * string representing the filename
+ *
+ * @text_content: A pointer to a character
+ * string representing the text to be written
+ *
+ * Return: On success, returns 1. On failure, returns -1.
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wr, len = 0;
+	int fd, w, len = 0;
 
-	if (filename)
+	/* Check if filename is NULL */
+	if (filename == NULL)
 		return (-1);
 
-	if (!text_content)
+	/* Get length of the text_content string */
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
 
+	/* Open file with read/write access and create it if it doesn't exist */
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = write(fd, text_content, len);
 
-	if (fd == -1 || wr == -1)
+	/* Write the text_content to the file */
+	w = write(fd, text_content, len);
+
+	/* Check if file or write operation failed */
+	if (fd == -1 || w == -1)
 		return (-1);
 
+	/* Close the file */
 	close(fd);
 
+	/* Return success */
 	return (1);
 }
